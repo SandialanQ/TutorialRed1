fdb7e3806687a8438d25182491627bb37ec63fa4
Alan Ramos <alan26ramos26@gmail.com>
2025-08-27 20:46:42 -0600

    Cambios en archivo lectura

--- DataVisualization.py
+++ DataVisualization.py
@@ -37,7 +37,7 @@ plt.show()
 test_v=np.reshape(imtest,(784,1)) #La acomodamos como matrix (784,1) para poder usarla en la red
 
 #cargamos nuestra red:
-archivo_lectura = open("red_prueba.pkl",'rb')
+archivo_lectura = open("red_prueba1.pkl",'rb')
 net = pickle.load(archivo_lectura)
 archivo_lectura.close()
 
